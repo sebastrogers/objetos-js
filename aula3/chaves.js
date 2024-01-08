f777f0dcde57ef8f5175159f96bc0429ac9b6c62
Sebastiao Rogerio <sebast.rogers@gmail.com>
2024-01-08 16:54:43 -0300

    exemplos com JSON

--- aula3/chaves.js
+++ aula3/chaves.js
@@ -5,14 +5,14 @@ const cliente = {
     telefone: ["1155555550", "1144444440"],
   };
   
-  // cliente.enderecos = [
-  //  {
-  //    rua: "R. Joseph Climber",
-  //    numero: 1337,
-  //    apartamento: true,
-  //    complemento: "ap 934",
-  //  },
-  //];
+  cliente.enderecos = [
+   {
+     rua: "R. Joseph Climber",
+     numero: 1337,
+     apartamento: true,
+     complemento: "ap 934",
+   },
+  ];
   
   const chavesDoObjeto = Object.keys(cliente);
   
